7efdbd6f5ca8e9f24d2ba6b831db167a80790436
zhouyuan <z244370114@outlook.com>
2023-09-27 15:40:36 +0800

    1.更新代码；

--- android/app/src/main/kotlin/com/xhx/downloaderx/MainActivity.kt
+++ android/app/src/main/kotlin/com/xhx/downloaderx/MainActivity.kt
@@ -3,4 +3,10 @@ package com.xhx.downloaderx
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.plugins.add(MethodPlugin())
+    }
+
 }
