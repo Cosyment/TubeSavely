18182900f66d3fb1bf8c98a97b93523d13fbcf20
yzhou <yzhou@amxc.tech>
2023-10-13 22:48:18 +0800

    1.添加登录功能; 2.网络框架；

---
+++ lib/network/http_api.dart
@@ -0,0 +1,9 @@
+class HttpApi {
+
+  static String baseUrl = 'https://openai.api.firefix.cn';
+
+  ///版本更新
+  static String updateApp = "/platForm/App/updateCustom.json";
+
+
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-
 import '../constants/colors.dart';
-import '../widget/primary_button.dart';
+import '../network/http_utils.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -14,7 +13,17 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late TextEditingController emailController;
+  late TextEditingController codeController;
+
   var isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController(text: "244370114@qq.com");
+    codeController = TextEditingController(text: "");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +57,7 @@ class _LoginPageState extends State<LoginPage> {
                 height: 88.h,
                 margin: EdgeInsets.symmetric(vertical: 40.h),
                 child: TextField(
+                  controller: emailController,
                   autofocus: true,
                   keyboardType: TextInputType.emailAddress,
                   inputFormatters: <TextInputFormatter>[
@@ -88,47 +98,86 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
               ),
-              Container(
-                height: 88.h,
-                child: TextField(
-                  keyboardType: TextInputType.visiblePassword,
-                  inputFormatters: <TextInputFormatter>[
-                    LengthLimitingTextInputFormatter(16)
-                  ],
-                  style: TextStyle(color: primaryColor, fontSize: 28.sp),
-                  decoration: InputDecoration(
-                    hintText: "Enter your password",
-                    labelText: "Password",
-                    hintStyle: TextStyle(
-                      fontSize: 26.sp,
-                      color: Colors.grey,
-                    ),
-                    labelStyle: TextStyle(fontSize: 26.sp, color: Colors.grey),
-                    filled: true,
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(14.r)),
-                      borderSide: BorderSide(
-                        color: Colors.grey,
-                        width: 1.w,
-                      ),
-                    ),
-                    disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(14.r)),
-                      borderSide: BorderSide(
-                        color: primaryColor,
-                        width: 1.w,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(14.r)),
-                      borderSide: BorderSide(
-                        color: primaryColor,
-                        width: 4.w,
+              Stack(
+                children: [
+                  Container(
+                    height: 88.h,
+                    child: TextField(
+                      controller: codeController,
+                      keyboardType: TextInputType.visiblePassword,
+                      inputFormatters: <TextInputFormatter>[
+                        LengthLimitingTextInputFormatter(16)
+                      ],
+                      style: TextStyle(color: primaryColor, fontSize: 28.sp),
+                      decoration: InputDecoration(
+                        hintText: "Enter your password",
+                        labelText: "Password",
+                        hintStyle: TextStyle(
+                          fontSize: 26.sp,
+                          color: Colors.grey,
+                        ),
+                        labelStyle:
+                            TextStyle(fontSize: 26.sp, color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(14.r)),
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                            width: 1.w,
+                          ),
+                        ),
+                        disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(14.r)),
+                          borderSide: BorderSide(
+                            color: primaryColor,
+                            width: 1.w,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(14.r)),
+                          borderSide: BorderSide(
+                            color: primaryColor,
+                            width: 4.w,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                  Positioned(
+                    right: 20.w,
+                    top: 10.w,
+                    child: InkWell(
+                      onTap: () async {
+                        sendMsm();
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20.w,
+                          horizontal: 20.w,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.r),
+                          gradient: const LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Color(0xFFFC6AEC),
+                              Color(0xFF7776FF),
+                            ],
+                          ),
+                        ),
+                        child: Text(
+                          "发送验证码",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 26.sp,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
               SizedBox(height: 12.w),
               Container(
@@ -149,6 +198,7 @@ class _LoginPageState extends State<LoginPage> {
                   backgroundColor: primaryColor,
                   shape: const CircleBorder(),
                   onPressed: () {
+                    login();
                     setState(() {
                       isLoading = true;
                     });
@@ -169,5 +219,23 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+  }
+
+  login() async {
+    var map = <String, dynamic>{};
+    map['email'] = emailController.text;
+    map['code'] = codeController.text;
+    var requestNetWorkAy = await HttpUtils.instance
+        .requestNetWorkAy(Method.get, "/tUser/login", queryParameters: map);
+    setState(() {});
+  }
+
+  sendMsm() async {
+    var map = <String, dynamic>{};
+    map['email'] = emailController.text;
+    var requestNetWorkAy = await HttpUtils.instance.requestNetWorkAy(
+        Method.get, "/tVerCode/sendCode",
+        queryParameters: map);
+    setState(() {});
   }
 }
