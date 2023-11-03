d94c65b9af45299e16a2f49d21b264ea8c206a19
pym <18207179424@163.com>
2023-11-03 18:15:01 +0800

    启动页优化

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:downloaderx/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'dart:async';
 import 'package:shimmer/shimmer.dart';
 
@@ -11,42 +12,47 @@ class SplashPage extends StatefulWidget {
   _SplashPageState createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage>   with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _jumpAnimation;
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _translationAnimationController;
+  late Animation<Offset> _offsetAnimation;
+  bool isShowTitle = false;
+
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
+    // 创建动画控制器
+    _translationAnimationController = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 1000),
+      duration: Duration(milliseconds: 900),
     );
-    _jumpAnimation = Tween<double>(begin: 0, end: 30).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeOut,
-      ),
-    );
-    _animationController.addStatusListener((status) {
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(0, 0),
+      end: const Offset(0, -200),
+    ).animate(_translationAnimationController);
+    _translationAnimationController.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
-        _animationController.reverse();
-      } else if (status == AnimationStatus.dismissed) {
-        _animationController.forward();
+        setState(() {
+          isShowTitle = true;
+        });
       }
     });
-    _animationController.forward();
+
+    _translationAnimationController.forward();
     _mockCheckForSession().then((status) {
       Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (BuildContext context) => MainPage()));
     });
   }
+
   @override
   void dispose() {
-    _animationController.dispose();
+    _translationAnimationController.dispose();
     super.dispose();
   }
+
   Future<bool> _mockCheckForSession() async {
-    await Future.delayed(Duration(milliseconds: 6000), () {});
+    await Future.delayed(Duration(milliseconds: 2500), () {});
     return true;
   }
 
@@ -56,53 +62,71 @@ class _SplashPageState extends State<SplashPage>   with SingleTickerProviderStat
         const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return Scaffold(
       backgroundColor: Color(0XFF26242e),
-      body: Stack(children: [
-        Positioned(
-            child: Center(
-          child: Shimmer.fromColors(
-      period: Duration(milliseconds: 1500),
-      baseColor: Color(0xFF8983F7),
-      highlightColor: Color(0xFFA3DAFB),
-      child: Container(
+      body: Stack(
         alignment: Alignment.center,
-        child:AnimatedBuilder(
-          animation: _jumpAnimation,
-          builder: (context, child) {
-            return Container(
-              margin: EdgeInsets.only(bottom: _jumpAnimation.value),
-              child: Image.asset(
-                'assets/ic_transparent_logo.png',
-                width: 255,
-                height: 255,
-              ),
-            );
-          },
-        ),
-      ),
-    )
-        )),
-        Positioned(
-          top: 280,
-            left: 0,
-            right: 0,
-            child: Shimmer.fromColors(
-          period: Duration(milliseconds: 1500),
-          baseColor: Color(0xFF8983F7),
-          highlightColor: Color(0xFFA3DAFB),
-          child: Container(
-            alignment: Alignment.center,
-            child: Text(
-              "TobeSaver",
-              style: TextStyle(fontSize: 36.0, shadows: <Shadow>[
-                Shadow(
-                    blurRadius: 18.0,
-                    color: Colors.black87,
-                    offset: Offset.fromDirection(120, 12))
-              ]),
-            ),
+        children: [
+          AnimatedBuilder(
+            animation: _translationAnimationController,
+            builder: (context, child) {
+              return Transform.translate(
+                offset: _offsetAnimation.value,
+                child: Center(
+                  child: Stack(alignment: Alignment.center, children: [
+                    Positioned(
+                      child: isShowTitle
+                          ? Shimmer.fromColors(
+                              period: Duration(milliseconds: 1000),
+                              baseColor: Color(0xFF8983F7),
+                              highlightColor: Color(0xFFA3DAFB),
+                              child: Container(
+                                child: Image.asset(
+                                  'assets/ic_transparent_logo.png',
+                                  width: 255,
+                                  height: 255,
+                                ),
+                              ),
+                            )
+                          : Image.asset(
+                              'assets/ic_transparent_logo.png',
+                              width: 255,
+                              height: 255,
+                            ),
+                    ),
+                    Positioned(
+                      top: 180,
+                      child: Visibility(
+                        visible: isShowTitle,
+                        child: Shimmer.fromColors(
+                          period: Duration(milliseconds: 1000),
+                          baseColor: Color(0xFF8983F7),
+                          highlightColor: Color(0xFFA3DAFB),
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: Text(
+                              "TobeSaver",
+                              style:
+                                  TextStyle(fontSize: 36.0, shadows: <Shadow>[
+                                Shadow(
+                                    blurRadius: 18.0,
+                                    color: Colors.black87,
+                                    offset: Offset.fromDirection(120, 12))
+                              ]),
+                            ).animate().fade(),
+                          ),
+                        ),
+                      ),
+                    )
+                  ]),
+                ),
+              );
+            },
           ),
-        ))
-      ]),
+          Positioned(
+            child: Text("创作高质量视频"),
+            bottom: 60,
+          )
+        ],
+      ),
     );
   }
 }
