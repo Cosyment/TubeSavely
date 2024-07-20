15ae6879c1986fb0dd9a79bc9d44f80c38f3c816
Waiting <waitinghc@foxmail.com>
2024-07-21 01:17:26 +0800

    完善dmg打包脚本

--- build-dmg.sh
+++ build-dmg.sh
@@ -1,22 +1,48 @@
 #!/bin/sh
-# 检查是否存在 Application-Installer.dmg 文件，如果存在则删除
-#if [ -f Application-Installer.dmg ]; then
-#    rm Application-Installer.dmg
-#fi
-#
-#test -f Application-Installer.dmg && rm Application-Installer.dmg
-#create-dmg \
-#  --volname "Application Installer" \
-#  --volicon "build/macos/Build/Products/Release/TubeSavely.app/Contents/Resources/AppIcon.icns" \
-#  --background "assets/images/ic_feedback.png" \
-#  --window-pos 200 120 \
-#  --window-size 800 400 \
-#  --icon-size 100 \
-#  --icon "assets/ic_logo_small.png" 200 190 \
-#  --hide-extension "TubeSavely.app" \
-#  --app-drop-link 600 185 \
-#  "build/macos/TubeSavely.dmg" \
-#  "build/macos/Build/Products/Release/TubeSavely.app"
+set -e
 
-hdiutil create -srcfolder build/macos/Build/Products/Release/TubeSavely.app TubeSavely.dmg
-hdiutil convert TubeSavely.dmg -format UDBZ -o Compressed_TubeSavely.dmg
+# 设置变量
+APP_NAME="TubeSavely"  # 你的应用名称
+APP_VERSION=$(grep "version:" pubspec.yaml | awk '{print $2}')  # 从pubspec.yaml读取版本号
+FLUTTER_PROJECT_PATH="/Users/Waiting/AndroidStudioProjects/TubeSavely"  # Flutter项目路径，当前目录
+BUILD_PATH="${FLUTTER_PROJECT_PATH}/build/macos/Build/Products/Release"  # 构建输出路径
+APP_PATH="${BUILD_PATH}/${APP_NAME}.app"  # 应用的完整路径
+ICON_PATH="${APP_PATH}/Contents/Resources/AppIcon.icns"  # 应用图标路径
+DMG_NAME="${APP_NAME}_${APP_VERSION}.dmg"  # DMG文件名
+DMG_PATH="${BUILD_PATH}/${DMG_NAME}"  # DMG文件存放路径
+TMP_DIR=$(mktemp -d)  # 创建临时目录
+
+# 确保应用已经构建
+echo "确保macOS应用已经构建在路径：${APP_PATH}"
+if [ ! -d "${APP_PATH}" ]; then
+  echo "构建macOS应用..."
+  cd "$FLUTTER_PROJECT_PATH" || exit
+  flutter build macos --release
+fi
+
+# 复制.app文件到临时目录
+echo "复制应用文件到临时目录..."
+cp -R "${APP_PATH}" "${TMP_DIR}"
+
+# 进入临时目录
+cd "${TMP_DIR}" || exit
+
+# 使用create-dmg创建DMG
+echo "使用create-dmg创建DMG文件：${DMG_NAME}"
+create-dmg \
+  --volname "${APP_NAME}" \
+  --window-size 800 400 \
+  --background "$FLUTTER_PROJECT_PATH/assets/images/ic_feedback.png" \
+  --icon-size 100 \
+  --text-size 12 \
+  --icon "${APP_NAME}.app" 200 150 \
+  --app-drop-link 600 150 \
+  "${DMG_PATH}" \
+  "${APP_NAME}.app"
+
+# 清理临时目录
+echo "清理临时文件..."
+cd "$FLUTTER_PROJECT_PATH" || exit
+rm -rf "${TMP_DIR}"
+
+echo "DMG打包完成：${DMG_PATH}"
